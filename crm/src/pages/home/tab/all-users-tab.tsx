70f127475ad5c8831da132314c4789bc66a809c4
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2025-11-17 21:37:19 +0530

    feat: add loading state for AllUsersTab component when apiKey is not provided

--- crm/src/pages/home/tab/all-users-tab.tsx
+++ crm/src/pages/home/tab/all-users-tab.tsx
@@ -6,6 +6,9 @@ interface AllUsersTabProps {
 }
 
 export const AllUsersTab: React.FC<AllUsersTabProps> = ({ apiKey }) => {
+  if (!apiKey) {
+    return <div className="p-4 text-app-secondary">Loading...</div>;
+  }
   return (
     <div className="p:0 lg:p-3">
       <AllUsersTable apiKey={apiKey} />
